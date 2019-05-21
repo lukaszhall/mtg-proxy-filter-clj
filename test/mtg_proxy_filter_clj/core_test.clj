8461fa78c30a603ae5559aa5c12b4ee0becca204
lukaszhall <hall@alum.mit.edu>
2019-05-21 17:07:23 -0400

    init

---
+++ test/mtg_proxy_filter_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns mtg-proxy-filter-clj.core-test
+  (:require [clojure.test :refer :all]
+            [mtg-proxy-filter-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
